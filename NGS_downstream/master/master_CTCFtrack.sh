a94c24487cd08880b1cc12e4c5c784658e0af6ea
Till-mpimg <74197498+Till-mpimg@users.noreply.github.com>
2021-03-09 19:34:01 +0100

    Add files via upload

---
+++ NGS_downstream/master/master_CTCFtrack.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Master script to identify CTCF binding sites for Figs. 7f-g and Sup. Figs 7d-e.
+work_dir=$(pwd)'/'
+path=''
+
+help() {
+   echo "Prepares CTCF .tsv file from FIMO for use in UCSC (to visualize CBS)."
+   echo
+   echo "Syntax: ./master_CTCFtrack.sh [-b|d|p|h]"
+   echo "options:"
+   echo "d     Provides working directory (Standard is current directory)."
+   echo "h     Prints this help."
+   echo "p     Provide path to /Gjaltema_paper/NGS_downstream. [mandatory]"
+   echo
+}
+
+parse_args() {
+    case "$1" in
+        -d)
+            work_dir="$2"
+            ;;
+        -p)
+            path="$2"
+            ;;
+        -h)
+            help
+            exit 0
+            ;;
+        *)
+            echo "Unknown or badly placed parameter '$1'." 1>&2
+            exit 1
+            ;;
+    esac
+}
+
+while [[ "$#" -ge 1 ]]; do
+    parse_args "$1" "$2"
+    shift; shift
+done
+
+if [[ $path == '' ]]
+then
+	echo -e "Please provide the path to /Xert_paper/NGS_downstream with -p"
+  exit 1
+fi
+
+work_dir=$(realpath $work_dir)'/'
+path=$(realpath $path)'/'
+
+# Uses .tsv file from FIMO to create colored BED track for UCSC
+echo -e "Generating CBS track"
+Rscript ${path}scripts/generate_CBS_bed.R ${path}files/ $work_dir
